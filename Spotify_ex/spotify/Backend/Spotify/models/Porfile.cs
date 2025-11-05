43d7eaef35deda9abc16878bf8aa1c6fa9edde6e
LlucEspunya <lespuna562@boscdelacoma.cat>
2025-11-05 17:23:26 +0100

    Fitxer model perfils

---
+++ Spotify_ex/spotify/Backend/Spotify/models/Porfile.cs
@@ -0,0 +1,11 @@
+namespace Spotify.Model;
+
+public class Porfile
+{
+    public Guid Id { get; set; }
+    
+    public string Descritpion { get; set; }
+    
+    public string Status { get; set; }   
+    
+}
