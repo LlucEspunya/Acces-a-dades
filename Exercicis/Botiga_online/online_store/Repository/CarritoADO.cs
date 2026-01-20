ed62743b99f1f5cb94391d78607012d2805ccef4
LlucEspunya <lespuna562@boscdelacoma.cat>
2026-01-20 16:28:32 +0100

    Modificacions carritoADO per fer un select relacionant Productes i Carritos afegint un INNER JOIN en el endpoint GetAll

--- Exercicis/Botiga_online/online_store/Repository/CarritoADO.cs
+++ Exercicis/Botiga_online/online_store/Repository/CarritoADO.cs
@@ -17,34 +17,38 @@ static class CarritoADO
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         cmd.Parameters.AddWithValue("@ID", carrito.ID);
-        cmd.Parameters.AddWithValue("@name", carrito.ID);
+        cmd.Parameters.AddWithValue("@name", carrito.name);
 
         int rows = cmd.ExecuteNonQuery();
         Console.WriteLine($"{rows} fila inserida.");
         dbConn.Close();
     }
 
-    public static List<Carrito> GetAll(DatabaseConnection dbConn)
+    public static List<ResumeProduct> GetAll(DatabaseConnection dbConn, Guid ID )
     {
-        List<Carrito> Carritos = new();
+        List<ResumeProduct> ResumeProducts = new();
 
         dbConn.Open();
-        string sql = "SELECT ID, name FROM Carritos";
+        string sql = "SELECT cp.ID, Carrito_id, Product_id, Quantity, p.Price FROM CarritoProduct as cp INNER JOIN Products as p on p.ID = cp.Product_id WHERE Carrito_id = @ID";
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+         cmd.Parameters.AddWithValue("@ID", ID);
         using SqlDataReader reader = cmd.ExecuteReader();
 
         while (reader.Read())
         {
-            Carritos.Add(new Carrito
+            ResumeProducts.Add(new ResumeProduct
             {
                 ID = reader.GetGuid(0),
-                name = reader.GetString(1),
+                Carrito_id = reader.GetGuid(1),
+                Product_id = reader.GetGuid(2),
+                Quantity = reader.GetInt32(3),
+                Price = reader.GetFloat(4),
             });
         }
 
         dbConn.Close();
-        return Carritos;
+        return ResumeProducts;
     }
 
     public static Carrito? GetById(DatabaseConnection dbConn, Guid ID)
@@ -63,7 +67,7 @@ static class CarritoADO
             carrito = new Carrito
             {
                 ID = reader.GetGuid(0),
-                name = reader.GetString(2),
+                name = reader.GetString(1),
             };
         }
 
